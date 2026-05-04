204ef8ecffb29d80a2fb04b6bb4d2ee74dd228c9
Noxamics <noxamics@gmail.com>
2026-05-04 21:19:50 +0700

    fitur laporan perkembangan 7 hari

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\Api\PrediksiController;
 use App\Http\Controllers\Api\AnalyticsController;
 use App\Http\Controllers\Api\AdminController;
 use App\Http\Controllers\Api\PasswordResetController;
-use App\Http\Controllers\PredictController; 
+use App\Http\Controllers\PredictController;
 use App\Http\Controllers\SurveyController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LaporanController;
 
 /*
 |══════════════════════════════════════════════════════════════
@@ -98,6 +99,8 @@ Route::middleware('jwt.auth')->group(function () {
         Route::get('comparison', [AnalyticsController::class, 'comparison']);
         Route::get('history', [AnalyticsController::class, 'history']);
     });
+
+    Route::get('/laporan', [LaporanController::class, 'getLaporan']);
 
 });
 
